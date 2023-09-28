53a88917b04e030950adf76e9e9b9a7b16ec2ac2
Nichollas de Farias Leonides dos Santos <144858603+NichollasFarias@users.noreply.github.com>
2023-09-28 16:30:10 -0300

    Add files via upload

---
+++ Volume Esfera/PrjEx05_33574/frmEx05_33574.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PrjEx05_33574
+{
+    partial class frmEx05_33574
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmEx05_33574));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btmCal = new System.Windows.Forms.Button();
+            this.btmLimp = new System.Windows.Forms.Button();
+            this.btmSai = new System.Windows.Forms.Button();
+            this.txtDia = new System.Windows.Forms.TextBox();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Volume ○";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(12, 262);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Diametro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(12, 354);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Resposta";
+            // 
+            // btmCal
+            // 
+            this.btmCal.Location = new System.Drawing.Point(260, 284);
+            this.btmCal.Name = "btmCal";
+            this.btmCal.Size = new System.Drawing.Size(125, 51);
+            this.btmCal.TabIndex = 1;
+            this.btmCal.Text = "Calcular";
+            this.btmCal.UseVisualStyleBackColor = true;
+            this.btmCal.Click += new System.EventHandler(this.btmCal_Click);
+            // 
+            // btmLimp
+            // 
+            this.btmLimp.Location = new System.Drawing.Point(355, 341);
+            this.btmLimp.Name = "btmLimp";
+            this.btmLimp.Size = new System.Drawing.Size(125, 51);
+            this.btmLimp.TabIndex = 2;
+            this.btmLimp.Text = "Limpar";
+            this.btmLimp.UseVisualStyleBackColor = true;
+            this.btmLimp.Click += new System.EventHandler(this.btmLimp_Click);
+            // 
+            // btmSai
+            // 
+            this.btmSai.Location = new System.Drawing.Point(447, 398);
+            this.btmSai.Name = "btmSai";
+            this.btmSai.Size = new System.Drawing.Size(125, 51);
+            this.btmSai.TabIndex = 3;
+            this.btmSai.Text = "Sair";
+            this.btmSai.UseVisualStyleBackColor = true;
+            this.btmSai.Click += new System.EventHandler(this.btmSai_Click);
+            // 
+            // txtDia
+            // 
+            this.txtDia.Location = new System.Drawing.Point(13, 290);
+            this.txtDia.Name = "txtDia";
+            this.txtDia.Size = new System.Drawing.Size(100, 32);
+            this.txtDia.TabIndex = 0;
+            // 
+            // txtRes
+            // 
+            this.txtRes.Enabled = false;
+            this.txtRes.Location = new System.Drawing.Point(13, 381);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(105, 32);
+            this.txtRes.TabIndex = 7;
+            this.txtRes.TabStop = false;
+            // 
+            // frmEx05_33574
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(584, 461);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.txtDia);
+            this.Controls.Add(this.btmSai);
+            this.Controls.Add(this.btmLimp);
+            this.Controls.Add(this.btmCal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmEx05_33574";
+            this.Text = "Volume";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btmCal;
+        private System.Windows.Forms.Button btmLimp;
+        private System.Windows.Forms.Button btmSai;
+        private System.Windows.Forms.TextBox txtDia;
+        private System.Windows.Forms.TextBox txtRes;
+    }
+}
+
